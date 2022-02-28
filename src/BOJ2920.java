a9ad4d7f435e6cb3a92fd469a2907d16e9941168
Munbin-Lee <3412mb@gmail.com>
2022-03-01 00:01:05 +0900

    test

---
+++ src/BOJ2920.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ2920 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int[] arr = new int[8];
+
+        for (int i = 0; i < 8; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] arrAscend = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] arrDescend = {8, 7, 6, 5, 4, 3, 2, 1};
+
+        if (Arrays.equals(arr, arrAscend)) {
+            System.out.println("ascending");
+        } else if (Arrays.equals(arr, arrDescend)) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+
+    }
+}
