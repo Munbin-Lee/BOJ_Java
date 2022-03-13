6a0ba42cfdc252bf11869c5c99cd654510349db7
Munbin-Lee <3412mb@gmail.com>
2022-03-14 00:25:18 +0900

    20220312

---
+++ src/BOJ1181.java
@@ -0,0 +1,24 @@
+//다시풀기
+
+import java.io.*;
+
+public class BOJ1181 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+        //Arrays.sort(arr);
+        //1.길이순으로
+        //2.사전순으로
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
