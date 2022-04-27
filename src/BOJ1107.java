36e6bc98e575bfdcaa5c6934dda06e22c5731fb7
MB <123>
2022-04-27 13:43:27 +0900

    20020427

--- src/BOJ1107.java
+++ src/BOJ1107.java
@@ -11,13 +11,23 @@ public class BOJ1107 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        @SuppressWarnings("MismatchedReadAndWriteOfArray")
         boolean[] isBroken = new boolean[10];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         while (M-- > 0) {
             isBroken[Integer.parseInt(st.nextToken())] = true;
         }
         int min = Math.abs(N - 100); //+,-만 사용
+
+        loop:
+        for (int i = 0; i <= 999999; i++) {
+            String[] arr = String.valueOf(i).split("");
+            for (String j : arr) {
+                if (isBroken[Integer.parseInt(j)]) {
+                    continue loop;
+                }
+            }
+            min = Math.min(min, Math.abs(N - i) + arr.length);
+        }
         System.out.println(min);
     }
 }
